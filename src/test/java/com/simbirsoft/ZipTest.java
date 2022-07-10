5fc04f614bb22e225f8d77486b17e0b423a9cc08
AleksandrDankov <dankoffalexander@gmail.com>
2022-07-11 01:23:30 +0500

    Remove file 5MB, add 2MB.zip

--- src/test/java/com/simbirsoft/ZipTest.java
+++ src/test/java/com/simbirsoft/ZipTest.java
@@ -18,7 +18,7 @@ public class ZipTest {
 
     @Test
     public void zipTestWithPassword() throws Exception {
-        File zipSource = new File("src/test/resources/zip_5MB.zip");
+        File zipSource = new File("src/test/resources/zip_2MB.zip");
         try(InputStream is = new FileInputStream(zipSource)) {
             ZipInputStream zis = new ZipInputStream(is, "123".toCharArray());
             LocalFileHeader entry;
@@ -31,11 +31,11 @@ public class ZipTest {
 
             Assertions.assertAll(
                     () -> assertThat(fileNames.size()).isEqualTo(5),
-                    () -> assertThat(fileNames.get(0)).isEqualTo("file_example_PNG_2500kB.jpg"),
+                    () -> assertThat(fileNames.get(0)).isEqualTo("file_example_ODS_5000.ods"),
                     () -> assertThat(fileNames.get(1)).isEqualTo("file_example_PPT_1MB.ppt"),
-                    () -> assertThat(fileNames.get(2)).isEqualTo("file-example_PDF_1MB.pdf"),
-                    () -> assertThat(fileNames.get(3)).isEqualTo("file-sample_1MB.doc"),
-                    () -> assertThat(fileNames.get(4)).isEqualTo("file_example_ODS_5000.ods")
+                    () -> assertThat(fileNames.get(2)).isEqualTo("file-sample_1MB.doc"),
+                    () -> assertThat(fileNames.get(3)).isEqualTo("sample.pdf"),
+                    () -> assertThat(fileNames.get(4)).isEqualTo("SampleText.txt")
             );
         }
     }
